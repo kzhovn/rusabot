ec433e7e3d96fd3ad7f7e123ace1dbdf64e68c1b
kzhovn <kzhovn@gmail.com>
2023-06-18 23:12:43 -0700

    Edit the .list message

--- rusabot.py
+++ rusabot.py
@@ -7,39 +7,59 @@ import discord
 from discord.ext.commands import Bot
 
 todo_file = 'todo.pkl'
-completed_todo_file = 'completed_todos.pkl'
+
+async def get_message(message_id, channel_id):
+    return await rusabot.get_channel(channel_id).fetch_message(message_id)
 
 class Todo:
     def __init__(self, message):
         self.text = message.content[2:].strip()
-        self.id = message.id
+        self.message_id = message.id
         self.url = message.jump_url
 
     def __repr__(self) -> str:
-        return f"<Message: {self.text}, ID: {self.id}>"
+        return f"<Message: {self.text}, ID: {self.message_id}>"
+
+    def compose_line(self):
+        return f"- {self.text} ({self.url})\n"
+
 
 class TodoList:
     def __init__(self):
         self.todos = {} # id int -> Todo
+        self.last_list_id = None
+        self.last_list_channel = None
 
     def add_todo(self, message: discord.Message):
         self.todos[message.id] = Todo(message)
         pickle.dump(self.todos, open(todo_file, 'wb'))
         print("Adding " + self.todos[message.id].text)
 
-    def remove_todo(self, message: discord.Message):
+    async def remove_todo(self, message: discord.Message):
+        todo = self.todos[message.id]
         if user_todos.todos.pop(message.id, None):
-            print("Removing " + message.content)
+            print("Removing " + todo.text)
             pickle.dump(self.todos, open(todo_file, 'wb'))
-            # completed_user_todos.add_todo(message)
-            # pickle.dump(completed_user_todos.todos, open(completed_todo_file, 'wb'))
+
+            if self.last_list_id:
+                message = await get_message(self.last_list_id, self.last_list_channel)
+                new_message_content = ""
+                for line in message.content.splitlines(keepends = True):
+                    print(line)
+                    print(todo.compose_line())
+                    if line.strip() == todo.compose_line().strip():
+                        new_message_content += ("✅" + line[1:])
+                    else:
+                        new_message_content += (line)
+
+                await message.edit(content=new_message_content)
         else:
             print(f"{message} was not a todo")
 
     def pretty_print(self) -> str:
         todo_str_list = ""
         for todo in self.todos.values():
-            todo_str_list = todo_str_list + f"- {todo.text} ({todo.url})\n"
+            todo_str_list = todo_str_list + todo.compose_line()
 
         return todo_str_list
 
@@ -48,18 +68,13 @@ class TodoList:
 
 rusabot = Bot(command_prefix = ".", intents=discord.Intents.all())
 user_todos = TodoList()
-completed_user_todos = TodoList()
 
-#when starting bot
 @rusabot.event
 async def on_ready():
     print("rusabot online")
     await rusabot.change_presence(activity = discord.Game("Testing")) #set status
     if os.path.isfile(todo_file):
         user_todos.todos = pickle.load(open(todo_file, 'rb'))
-    if os.path.isfile(completed_todo_file):
-        completed_user_todos.todos = pickle.load(open(completed_todo_file, 'rb'))
-
 
 #https://stackoverflow.com/questions/49331096/why-does-on-message-stop-commands-from-working
 @rusabot.event
@@ -71,14 +86,17 @@ async def on_message(message):
 
 @rusabot.event #check every new reaction (on_reaction_add only looks into cache)
 async def on_raw_reaction_add(payload):
-    message = await rusabot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+    message = await get_message(payload.message_id, payload.channel_id)
     if (payload.emoji.name == "✅" or payload.emoji.name == "❌") and is_todo(message):
-        user_todos.remove_todo(message)
+        await user_todos.remove_todo(message)
 
 #on .list, print current todos in channel
 @rusabot.command()
 async def list(context):
-    await context.message.channel.send(user_todos.pretty_print())
+    new_list = await context.message.channel.send(user_todos.pretty_print())
+    user_todos.last_list_id = new_list.id
+    user_todos.last_list_channel = context.message.channel.id
+
 
 def is_todo(message) -> bool:
     if message.content.startswith("--") and not message.author.bot:
