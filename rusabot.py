f08e0bf221bcdff5f39525f3dd344cff582790a6
kzhovn <kira.zhovnirovskii@gmail.com>
2025-01-15 22:01:57 -0800

    Rmv beeminder for real this time

--- rusabot.py
+++ rusabot.py
@@ -3,7 +3,6 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext.commands import errors
 from todo import TodoCog, TodoList, Todo
-from beeminder import Beeminder
 from bot_token import BOT_TOKEN
 
 rusabot = Bot(command_prefix = ".", intents=discord.Intents.all())
@@ -17,19 +16,17 @@ async def setup_hook():
     todolist_names_file = 'data/todolist_names.pkl'
     await rusabot.add_cog(TodoCog(rusabot, todolist_names_file))
 
-    await rusabot.add_cog(Beeminder())
-
 @rusabot.event
 async def on_command_error(ctx, error):
     if isinstance(error, errors.MissingRequiredArgument):
         await ctx.send("Missing an argument.")
     elif isinstance(error, errors.TooManyArguments):
-        await ctx.send("Too many arguments")       
+        await ctx.send("Too many arguments")
     elif isinstance(error, errors.CommandNotFound):
         return
     else:
         print(error)
-    
+
     await ctx.message.add_reaction("❌")
     await ctx.send_help(ctx.command)
 rusabot.run(BOT_TOKEN)
